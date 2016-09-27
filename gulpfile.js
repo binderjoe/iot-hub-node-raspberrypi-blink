d4d25c495b31d757f4f3ec652ba7efc1a1868b09
FAREAST\zhijzhao <zhijzhao@microsoft.com>
2016-09-27 10:50:31 +0800

    Add MIT license header for each js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,6 @@
-﻿require('gulp-common')(require('gulp'), 'raspberrypi-node', { appName: 'blink' });
+﻿/*
+* IoT Hub Raspberry Pi NodeJS Blink - Microsoft Sample Code - Copyright (c) 2016 - Licensed MIT
+*/
+'use strict';
+
+require('gulp-common')(require('gulp'), 'raspberrypi-node', { appName: 'blink' });
